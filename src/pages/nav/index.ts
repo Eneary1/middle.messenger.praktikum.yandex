c3ef3dc98c7bbc553cf8a288c1642538a6f054cb
Vitaliy Atlasov <vitaliatlasov@yandex.ru>
2022-11-02 11:56:55 +0300

    yo2

--- src/pages/nav/index.ts
+++ src/pages/nav/index.ts
@@ -59,3 +59,15 @@ function hashChanged() {
 
 window.addEventListener('load', hashChanged);
 window.addEventListener('hashchange', hashChanged);
+
+console.log("%c Список ссылок на все страницы ", "background-color:hsl(10,70%,45%)")
+console.table(    
+    [["Страница входа", "http://localhost:3000/#enter"],
+    ["Страница регистрации", "http://localhost:3000/#reg"],
+    ["Главная страница", "http://localhost:3000/#main"],
+    ["Стрница профиля", "http://localhost:3000/#profile"],
+    ["Изменение пароля", "http://localhost:3000/#pass"],
+    ["Изменение данных", "http://localhost:3000/#data"],
+    ["404", "http://localhost:3000/#404"],
+    ["500", "http://localhost:3000/#500"]]
+)
